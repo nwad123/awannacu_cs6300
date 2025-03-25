d874bfbed85af8e61edb50c14d57c3e66338ffac
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-25 12:46:06 -0600

    Getting close to finishing serial impl

--- src/serial/serial.cpp
+++ src/serial/serial.cpp
@@ -1,6 +1,7 @@
 #include "serial.hpp"
 #include "core.hpp"
 #include <fmt/core.h>
+#include <ranges>
 
 /// Returns 'true' when the points are visible to each other
 [[nodiscard]] 
@@ -8,21 +9,27 @@ auto check_visibility(const data_type data, index3 p1, index3 p2) -> bool;
 
 auto Serial::solve(const data_type data, const index3 at) -> output_type
 {
+    using namespace std::views;
+
     // get the height and width of the input data 
     const auto width = data.extent(0);
     const auto height = data.extent(1);
 
     // allocate some space for the output 
-    constexpr uint8_t TRUE = 1;
-    constexpr uint8_t FALSE = 0;
-    std::vector<uint8_t> output_vec(width * height, FALSE);
+    output_type output_vec(width * height, false);
 
     // make a little wrapper for the output with a span 
     auto output = Kokkos::mdspan(output_vec.data(), data.extents());
 
-    auto _ = check_visibility(data, {0, 0, 0}, {10, 0, 0});
+    // check visibility of each point to each other point 
+    for (const auto x : iota(0) | take(width)) {
+        for (const auto y : iota(0) | take(height)) {
+            const auto z = data(x, y);
+            output(x, y) = check_visibility(data, at, {x, y, z});
+        }
+    }
 
-    return {};
+    return output_vec; 
 }
 
 auto check_visibility(const data_type data, index3 p1, index3 p2) -> bool
@@ -45,19 +52,20 @@ auto check_visibility(const data_type data, index3 p1, index3 p2) -> bool
         auto p_1 = 2 * delta_abs.y - delta_abs.x;
         auto p_2 = 2 * delta_abs.z - delta_abs.x;
         while (p1.x != p2.x) {
-      p1.x += xs;
-      if (p_1 >= 0) {
-        p1.y += ys;
-        p_1 -= 2 * delta_abs.x;
-      }
-      if (p_2 >= 0) {
-        p1.z += zs;
-        p_2 -= 2 * delta_abs.x;
-      }
-      p_1 += 2 * delta_abs.y;
-      p_2 += 2 * delta_abs.z;
-            fmt::println("({}, {}, {})", p1.x, p1.y, p1.z);
-    }
+            p1.x += xs;
+            if (p_1 >= 0) {
+                p1.y += ys;
+                p_1 -= 2 * delta_abs.x;
+            }
+            if (p_2 >= 0) {
+                p1.z += zs;
+                p_2 -= 2 * delta_abs.x;
+            }
+            p_1 += 2 * delta_abs.y;
+            p_2 += 2 * delta_abs.z;
+            
+            if (p1.z < data(p1.x, p1.y)) { return false; }
+        }
     }
     else if (max == delta_abs.y) {
         const auto p1 = 2 * delta_abs.y - delta_abs.y;
@@ -70,5 +78,5 @@ auto check_visibility(const data_type data, index3 p1, index3 p2) -> bool
     
     }
 
-    return false;
+    return true;
 }
