7a07e833816e9b0aececa35eb777d32344e99ab8
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-25 12:25:53 -0600

    Added more to basic tests

---
+++ test/basic_images.hpp
@@ -0,0 +1,49 @@
+#include "mdspan.hpp"
+#include "core.hpp"
+#include <array>
+#include <utility>
+#include <ranges>
+
+namespace detail {
+
+// Returns an owning, stack based array paired with the 2d span over that array.
+// flat_plane_fn<5, 4>() -> {array, 2d_span}
+//
+// this image represents a purely flat plane. every location should be visible 
+// from every other location.
+template<size_t W, size_t H>
+[[nodiscard]]
+constexpr auto flat_plane_fn() -> std::pair<std::array<int16_t, W*H>, mat_2d_i16> {
+    auto arr = std::array<int16_t, W*H>{};
+    auto span = mat_2d_i16(arr.data(), W, H);
+
+    return {arr, span};
+}
+
+// Returns an owning, stack based array paired with the 2d span over that array.
+// sloped_plane_fn<5, 4>(1, 1) -> {array, 2d_span}
+//
+// The plane is sloped on the x-axis according to `x` and sloped on the y-axis 
+// according to `y`. There is no offset.
+//
+// Every location should be visible from every other location.
+template<size_t W, size_t H>
+[[nodiscard]]
+constexpr auto sloped_plane_fn(const int16_t x, const int16_t y) -> std::pair<std::array<int16_t, W*H>, mat_2d_i16> {
+    using namespace std::views;
+
+    auto arr = std::array<int16_t, W*H>{};
+    auto span = mat_2d_i16(arr.data(), W, H);
+
+    constexpr auto zero = static_cast<int16_t>(0);
+
+    for (const auto x_index : iota(zero) | take(W)) {
+        for (const auto y_index : iota(zero) | take(H)) {
+            span(x_index, y_index) = x * x_index + y + y_index;
+        }
+    }
+
+    return {arr, span};
+}
+
+}
--- test/solvers.cpp
+++ test/solvers.cpp
@@ -1,10 +1,17 @@
 #include "solvers.hpp"
 #include "core.hpp"
+#include "serial/serial.hpp"
 #include "solver/concepts.hpp"
+#include "basic_images.hpp"
 #include <gtest/gtest.h>
 #include <fmt/core.h>
 #include <filesystem>
 
+// ------ Test Images -----
+const auto [flat_plane_storage, flat_plane] = detail::flat_plane_fn<100, 100>();
+const auto [sloped_plane_storage, sloped_plane] = detail::sloped_plane_fn<100, 100>(1, 1);
+
+// ------ Test Functions -----
 template<Solver ...S>
 auto print_names([[maybe_unused]] S... solvers)
 {
@@ -12,6 +19,8 @@ auto print_names([[maybe_unused]] S... solvers)
     (fmt::println(" - {}", S::name), ...);
 }
 
-TEST(Serial, OriginalTest) {
-    std::filesystem::path("__temp__.raw");
+// ------ Tests -----
+TEST(Serial, FlatPlane) {
+    Serial serial;
+    auto output = serial.solve(flat_plane, {5, 5, 5});
 }
