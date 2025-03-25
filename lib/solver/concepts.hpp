a9944144bd57e036bc00caedc6dff431141dd393
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-24 23:58:19 -0600

    refactored to accept both span and mdspan impls

--- lib/solver/concepts.hpp
+++ lib/solver/concepts.hpp
@@ -1,15 +1,13 @@
 #pragma once 
 
-#include <span>
+#include "core.hpp"
 #include <string_view>
-#include <vector>
-
-using data_type = std::span<uint16_t>;
-// TODO: check if we can actually use std::vector<bool> or not
-using output_type = std::vector<bool>;
 
 template<class S>
 concept Solver = requires(S& s, const data_type data) {
+    { S::name } -> std::convertible_to<const std::string_view>;
+    { s.solve(data) } -> std::same_as<output_type>;
+} || requires(S& s, const std::span<int16_t> data) {
     { S::name } -> std::convertible_to<const std::string_view>;
     { s.solve(data) } -> std::same_as<output_type>;
 };
