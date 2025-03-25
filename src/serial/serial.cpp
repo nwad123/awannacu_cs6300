6614825b5350a62df22618a434003414a89b0994
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-25 16:29:58 -0600

    Basic implementation done?

--- src/serial/serial.cpp
+++ src/serial/serial.cpp
@@ -71,14 +71,44 @@ auto check_visibility(const data_type data, index3 p1, index3 p2) -> bool
         }
     }
     else if (max == delta_abs.y) {
-        const auto p1 = 2 * delta_abs.y - delta_abs.y;
-        const auto p2 = 2 * delta_abs.x - delta_abs.y;
+        auto p_1 = 2 * delta_abs.y - delta_abs.y;
+        auto p_2 = 2 * delta_abs.x - delta_abs.y;
 
+        while (p1.y != p2.y) {
+            p1.y += ys;
+            if (p_1 >= 0) {
+                p1.x += xs;
+                p_1 -= 2 * delta_abs.y;
+            }
+            if (p_2 >= 0) {
+                p1.z += zs;
+                p_2 -= 2 * delta_abs.y;
+            }
+            p_1 += 2 * delta_abs.x;
+            p_2 += 2 * delta_abs.z;
+            
+            if (p1.z < data(p1.x, p1.y)) { return false; }
+        }
     }
     else {
-        const auto p1 = 2 * delta_abs.y - delta_abs.z;
-        const auto p2 = 2 * delta_abs.z - delta_abs.z;
+        auto p_1 = 2 * delta_abs.y - delta_abs.z;
+        auto p_2 = 2 * delta_abs.z - delta_abs.z;
     
+        while (p1.z != p2.z) {
+            p1.z += zs;
+            if (p_1 >= 0) {
+                p1.y += ys;
+                p_1 -= 2 * delta_abs.z;
+            }
+            if (p_2 >= 0) {
+                p1.x += xs;
+                p_2 -= 2 * delta_abs.z;
+            }
+            p_1 += 2 * delta_abs.y;
+            p_2 += 2 * delta_abs.x;
+            
+            if (p1.z < data(p1.x, p1.y)) { return false; }
+        }
     }
 
     return true;
