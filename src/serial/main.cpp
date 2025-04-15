bca9d3f58bf24bdc84d78f421cfe14806ee510d2
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-15 16:47:00 -0600

    Checked to make sure we were reading in the values right

--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -1,7 +1,25 @@
 #include "core.hpp"
 #include "fmt/base.h"
+#include "span.hpp"
+#include <algorithm>
+#include <cstdlib>
 
-auto main() -> int
+[[nodiscard]]
+static inline auto bad_usage(const tcb::span<char*> args) -> int;
+
+auto main(int argc, char** argv) -> int
 {
-    fmt::println("Hello world!");
+    const tcb::span<char*> args = tcb::span(argv, static_cast<size_t>(argc));
+
+    if (args.size() < 3) { return bad_usage(args); }
+
+    auto h_ = read_input(args[1]);
+
+    fmt::println("Max: {}, Min: {}", *std::max_element(h_.cbegin(), h_.cend()), *std::min_element(h_.cbegin(), h_.cend()));
+}
+
+auto bad_usage(const tcb::span<char*> args) -> int 
+{
+    fmt::println("Usage: {} <input-file> <output-file>", args[0]);
+    return EXIT_FAILURE;
 }
