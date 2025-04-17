a09cd2b71e78f798040a2b614aed228ca2eb2af0
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-17 15:52:42 -0600

    refactoring parallel_cpu

--- src/parallel_cpu/parallel_cpu.hpp
+++ src/parallel_cpu/parallel_cpu.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "core.hpp"
+#include <vector>
+#include <cstdint>
+
+auto calculateVisibility(const std::vector<unsigned short>& height_map, 
+                         size_t width, size_t height, 
+                         int radius = 100, int angle = 12) -> std::vector<unsigned int>;
